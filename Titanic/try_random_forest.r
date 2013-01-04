4a089d3d5b55eea562c04535b21a05fd52eb85af
storni_remo_sandro <storni_remo_sandro@cyberagent.co.jp>
2013-01-04 20:18:21 +0900

    Titanic prediction competition

---
+++ Titanic/try_random_forest.r
@@ -0,0 +1,27 @@
+raw_trainset <- read.csv("train.csv")
+
+attach(raw_trainset)
+
+
+minimal <- data.frame(as.factor(survived), pclass, sex, age, sibsp, parch, fare, embarked)
+
+levels(minimal$embarked)[levels(minimal$embarked) == ""] <- NA
+colnames(minimal)[1] <- "survived"
+
+detach(raw_trainset)
+set.seed(111)
+minimal.imputed <- rfImpute(survived ~ ., minimal)
+
+set.seed(222)
+
+minimal.rf <- randomForest(survived ~ ., minimal.imputed)
+
+print(minimal.rf)
+
+raw_test_set <- read.csv("test.csv")
+attach(raw_test_set)
+
+test_set <- data.frame(pclass, sex, age, sibsp, parch, fare, embarked)
+test_set.roughfix <- na.roughfix(test_set)
+
+mypredict <- predict(minimal.rf, test_set.roughfix)
